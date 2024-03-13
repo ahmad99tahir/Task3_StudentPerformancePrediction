8f8acaa294b67fef94f648fdcfe79c48f87d7c6a
ahmad99tahir <154119368+ahmad99tahir@users.noreply.github.com>
2024-03-13 16:40:28 +0500

    updated path in main.py

--- Classification/main.py
+++ Classification/main.py
@@ -15,18 +15,20 @@ def main():
     lambda_reg = 1
 
     #This code is to perform Logistic Regression with Gradient Descent
+    """
     # Train the logistic regression gradient descent model using SMOTE data
     theta_trained_smote, cost_history_smote = train_model(X_train_smote, y_train_smote, alpha, num_iterations, 1)
     # Evaluate the logistic regression gradient descent model
     evaluate_model(X_test, y_test, theta_trained_smote, lambda_reg)
     #Plot the evaluations
     plot_evaluation_metrics(X_test, y_test, theta_trained_smote)
+    """
 
     #This code is to perform K-Fold CV for Logistic Regression Gradient Descent and save the best model
     """
     #let's apply K-Fold cross-validation to select the best logistic regression gradient descent model with original data
     best_model_simple = k_fold_cross_validation(X_train, y_train, alpha, num_iterations, lambda_reg)
-    joblib.dump(best_model_simple, 'models/logreg_gd_simple')
+    joblib.dump(best_model_simple, 'Classification/models/logreg_gd_simple')
     # Evaluate the logistic regression gradient descent model
     evaluate_model(X_test, y_test, best_model_simple, lambda_reg)
     #Plot the evaluations
@@ -37,7 +39,7 @@ def main():
     """
     #Train and Evaluate using original Data
     logreg = train_sklearn_logistic_regression(X_train,y_train)
-    joblib.dump(logreg,'models/logreg_simple')
+    joblib.dump(logreg,'Classification/models/logreg_simple')
     evaluate_model_sklearn(X_test,y_test,logreg)
     plot_evaluation_metrics_sklearn(X_test,y_test,logreg)
     """
@@ -46,23 +48,23 @@ def main():
     """
     #Train using smote data
     dt = train_decision_tree(X_train_smote,y_train_smote)
-    joblib.dump(dt,'models/tree_smote')
+    joblib.dump(dt,'Classification/models/tree_smote')
     evaluate_model_sklearn(X_test,y_test,dt)
     plot_evaluation_metrics_sklearn(X_test,y_test,dt)
     """
 
     #This code loads and evaluates a presaved gradient descent model.
-    """
-    model = joblib.load('models/logreg_gd_smote')
+    
+    model = joblib.load('Classification/models/logreg_gd_smote')
     # Evaluate the logistic regression gradient descent model
     evaluate_model(X_test, y_test, model, lambda_reg)
     #Plot the evaluations
     plot_evaluation_metrics(X_test, y_test, model)
-    """
+    
 
     #This code loads and evaluates a Logistic Regression or Decision Tree Model.
     """
-    model = joblib.load('models/tree_simple')
+    model = joblib.load('Classification/models/tree_simple')
     evaluate_model_sklearn(X_test,y_test,model)
     plot_evaluation_metrics_sklearn(X_test,y_test,model)
     """
